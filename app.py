c702842762f23056fb7dcb2cf03f64e77461467f
Tarundeep Singh <cyborg.tarun@gmail.com>
2024-04-22 12:06:44 +0530

    generate words nltk

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request,jsonify
 from flask_cors import CORS
 from utils import generate_image
+from utils import generate_image, generate_related_words
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +15,16 @@ def generate_image_api():
         return "Error: prompt parameter is required", 400
     image_path = generate_image(prompt)
     return send_file(image_path, as_attachment=True)
+
+@app.route('/generate-new-words', methods=['GET'])
+def generate_new_words():
+    data = request.args.get('words')
+    if not data:
+        return jsonify({'error': 'Invalid request data'}), 400
+   
+    new_gen_words=generate_related_words(data)
+    return jsonify({'new_gen_words': new_gen_words}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 from diffusers import AutoPipelineForText2Image
 import torch
+import nltk
+from nltk.corpus import wordnet as wn
+import random
+nltk.download('wordnet')
+
 
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
 pipe.to("cuda")
@@ -9,3 +14,31 @@ def generate_image(prompt):
     image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
     image.save(image_path)
     return image_path
+
+
+def generate_related_words(current_word):
+    final_list = []
+    
+    prompt_synsets = wn.synsets(current_word, pos=wn.NOUN)
+   
+ 
+    
+    related_words = set()
+    for synset in prompt_synsets:
+    # Hyponyms
+        for hyponym in synset.hyponyms():
+            related_words.update(lemma.name().replace('_', ' ') for lemma in hyponym.lemmas())
+ 
+       
+    if related_words:
+        final_list.append(random.sample(related_words, 3))
+ 
+    return final_list
+
+
+
+
+
+
+
+
